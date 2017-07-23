694b5867e467930da529161d34694198bbb76ad9
Chen Eshchar <chen@opinya.co.il>
2017-07-23 11:35:48 +0300

    fix: fix basic form. version: 0.0.3.

--- src/Model2Form.d.ts
+++
@@ -1,9 +0,0 @@
-import { Observable } from 'rxjs/Observable';
-import { FormGroup, FormArray, FormControl } from '@angular/forms';
-import 'rxjs/add/operator/map';
-export declare function ToNgForm<T>(this: Observable<T>, additionalControls?: any, validators?: any): Observable<FormGroup | FormArray | FormControl>;
-declare module 'rxjs/Observable' {
-    interface Observable<T> {
-        toNgForm: typeof ToNgForm;
-    }
-}
--- src/Model2Form.ts
+++ src/Model2Form.ts
@@ -2,23 +2,23 @@ import { Observable } from 'rxjs/Observable';
 import { FormGroup, FormArray, FormControl } from '@angular/forms';
 import 'rxjs/add/operator/map';
 
-export function ToNgForm<T>(
+declare module 'rxjs/Observable' {
+    interface Observable<T> {
+        toNgForm: typeof toNgForm;
+    }
+}
+
+export function toNgForm<T>(
     this: Observable<T>, 
     additionalControls?: any, 
     validators?: any
 ): Observable<FormGroup | FormArray | FormControl> {
-  return this.map(model => new FormGroup(
-      Object.keys(model).reduce((group, prop: string) => {
-        group[prop] = new FormControl(model[prop]);
-        return group;
-      }, {})
-  ));
+    return this.map(model => new FormGroup(
+        Object.keys(model).reduce((group, prop: string) => {
+            group[prop] = new FormControl(model[prop]);
+            return group;
+        }, {})
+    ));
 }
 
-Observable.prototype.toNgForm = ToNgForm;
-
-declare module 'rxjs/Observable' {
-  interface Observable<T> {
-    toNgForm: typeof ToNgForm;
-  }
-}
+Observable.prototype.toNgForm = toNgForm;
